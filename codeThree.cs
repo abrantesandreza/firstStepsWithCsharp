86c639326d30d5929cc17609e699fa3e2f5df95d
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-03-15 12:24:04 -0300

    freeCodeCamp: Working with Strings - concatenation, length, characters, indexes, methods

--- codeThree.cs
+++ codeThree.cs
@@ -7,9 +7,20 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Giraffe\nAcademy");
 
-            Console.ReadLine();
+            string phrase = "Giraffe Academy" + " is cool";
+            Console.WriteLine(phrase.Length); //23
+            Console.WriteLine(phrase.ToUpper()); //GIRAFFE ACADEMY IS COOL
+            Console.WriteLine(phrase.ToLower()); //giraffe academy is cool
+            Console.WriteLine(phrase.Contains("Academy")); //True
+            Console.WriteLine(phrase[3]); //a
+            Console.WriteLine(phrase.IndexOf("Academy")); //8
+            Console.WriteLine(phrase.IndexOf('f')); //4
+            Console.WriteLine(phrase.Substring(8)); //Academy
+            Console.WriteLine(phrase.Substring(8, 3)); //Aca
+
+
+            //Console.ReadLine();
         }
     }
 }
