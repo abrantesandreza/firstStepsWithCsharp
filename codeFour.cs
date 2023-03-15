e7b5b5be42415ef6ee25a3cfc9e14eab2a474689
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-03-15 12:31:51 -0300

    freeCodeCamp: Working with numbers

---
+++ codeFour.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(5 + 3); //8
+            Console.WriteLine(5 * 8); //40
+            Console.WriteLine(5 % 2); //1
+            Console.WriteLine(4 + 2 * 3); //10
+            Console.WriteLine((4 + 2) * 3); //18
+            Console.WriteLine(5.0 + 8.1); //13.1
+            Console.WriteLine(5 / 2); //2
+            Console.WriteLine(5 / 2.0); //2.5
+
+            //Console.ReadLine();
+        }
+    }
+}
