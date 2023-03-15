7a715d7752c0c794255382a7d0561e1d4e28d902
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-03-15 10:12:20 -0300

    freeCodeCamp: Working with Variables

---
+++ codeOne.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Stacey";
+            int characterAge;
+            characterAge = 73;
+
+            Console.WriteLine("There once was a woman named " + characterName);
+            Console.WriteLine("She was " + characterAge);
+            Console.WriteLine("She really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
