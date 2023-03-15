647757827b83bab11f2e92c09df935795d47967a
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-03-15 12:09:33 -0300

    freeCodeCamp: Working with Strings

---
+++ codeThree.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Giraffe Academy");
+
+            Console.ReadLine();
+        }
+    }
+}
