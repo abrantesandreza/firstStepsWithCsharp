5942e0fb7084690cb472b00a9f2f34eff692b1f3
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-03-15 10:32:16 -0300

    freeCodeCamp: Working with data types

---
+++ codeTwo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Working with basic data types";
+            char grade = 'A';
+            int age = 27;
+            double gpa = 4.3;
+            bool isApproved = true;
+
+            Console.ReadLine();
+        }
+    }
+}
