4b785b092244586397c390e888496b21d8bb96cf
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-03-17 10:46:41 -0300

    freeCodeCamp: While Loops

---
+++ codeFourteen.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int index = 10;
+            while (index >= 1)
+            { 
+                Console.WriteLine(index);
+                index--;
+            }
+
+        }
+
+
+    }
+
+}
+
