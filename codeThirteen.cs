0d322c42e31bbd293bb88e793efccdc6ea4d04c9
Andreza Abrantes <andrezadossantosabrantes@gmail.com>
2023-03-17 10:31:01 -0300

    freeCodeCamp: Switch Statements

---
+++ codeThirteen.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(GetDay(84));
+
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+
+                case 1:
+                    dayName = "Monday";
+                    break;
+
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+
+                case 3:
+                    dayName = "Wendesday";
+                    break;
+
+                case 4:
+                    dayName = "Thursday";
+                    break;
+
+                case 5:
+                    dayName = "Friday";
+                    break;
+
+                case 6:
+                    dayName = "Saturday";
+                    break;
+
+                default:
+                    dayName = "Invalid Number";
+                    break;
+            } 
+
+            return dayName;
+        }
+
+    }
+
+}
+
